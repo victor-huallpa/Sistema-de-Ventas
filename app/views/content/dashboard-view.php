6bc20ad7b58e7f81dc483c197cda995bd9bd85b9
victor-huallpa <vech.600@gmail.com>
2026-05-08 21:31:16 -0500

    feat: vista de dashboard

---
+++ app/views/content/dashboard-view.php
@@ -0,0 +1,71 @@
+            <div class="container is-fluid">
+                <h1 class="title">Home</h1>
+                <div class="columns is-flex is-justify-content-center">
+                    <figure class="image is-128x128">
+                        <img class="is-rounded" src="./fotos/default.png">
+                    </figure>
+                </div>
+                <div class="columns is-flex is-justify-content-center">
+                    <h2 class="subtitle">¡Bienvenido NOMBRE DE USUARIO!</h2>
+                </div>
+            </div>
+
+            <div class="container pb-6 pt-6">
+
+                <div class="columns pb-6">
+                    <div class="column">
+                        <nav class="level is-mobile">
+
+                            <div class="level-item has-text-centered">
+                                <a href="cashierList/">
+                                    <p class="heading"><i class="fas fa-cash-register fa-fw"></i> &nbsp; Cajas</p>
+                                    <p class="title">0</p>
+                                </a>
+                            </div>
+
+                            <div class="level-item has-text-centered">
+                                <a href="userList/">
+                                    <p class="heading"><i class="fas fa-users fa-fw"></i> &nbsp; Usuarios</p>
+                                    <p class="title">0</p>
+                                </a>
+                            </div>
+
+                            <div class="level-item has-text-centered">
+                                <a href="clientList/">
+                                    <p class="heading"><i class="fas fa-address-book fa-fw"></i> &nbsp; Clientes</p>
+                                    <p class="title">0</p>
+                                </a>
+                            </div>
+                        </nav>
+                    </div>
+                </div>
+
+                <div class="columns pt-6">
+                    <div class="column">
+                        <nav class="level is-mobile">
+
+                            <div class="level-item has-text-centered">
+                                <a href="categoryList/">
+                                  <p class="heading"><i class="fas fa-tags fa-fw"></i> &nbsp; Categorías</p>
+                                  <p class="title">0</p>
+                                </a>
+                            </div>
+
+                            <div class="level-item has-text-centered">
+                                <a href="productList/">
+                                    <p class="heading"><i class="fas fa-cubes fa-fw"></i> &nbsp; Productos</p>
+                                    <p class="title">0</p>
+                                </a>
+                            </div>
+
+                            <div class="level-item has-text-centered">
+                                <a href="saleList/">
+                                    <p class="heading"><i class="fas fa-shopping-cart fa-fw"></i> &nbsp; Ventas</p>
+                                    <p class="title">0</p>
+                                </a>
+                            </div>
+                        </nav>
+                    </div>
+                </div>
+                
+            </div>
