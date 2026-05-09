0b691a0811fd3ce5d75742a2cad3fa9cfe81720d
victor-huallpa <vech.600@gmail.com>
2026-05-08 21:29:36 -0500

    feat: navegacion principal

---
+++ app/views/inc/navBar.php
@@ -0,0 +1,20 @@
+<div class="full-width navBar">
+    <div class="full-width navBar-options">
+        <i class="fas fa-exchange-alt fa-fw" id="btn-menu"></i> 
+        <nav class="navBar-options-list">
+            <ul class="list-unstyle">
+                <li class="text-condensedLight noLink" >
+                    <a class="btn-exit" href="#" >
+                        <i class="fas fa-power-off"></i>
+                    </a>
+                </li>
+                <li class="text-condensedLight noLink" >
+                    <small>Usuario</small>
+                </li>
+                <li class="noLink">
+                    <img class="is-rounded img-responsive" src="<?php echo APP_URL?>app/views/fotos/default.png">
+                </li>
+            </ul>
+        </nav>
+    </div>
+</div> <!-- Fin NavBar -->
