a7838b97cd7674b99e8364957d21c19ddd496e48
victor-huallpa <vech.600@gmail.com>
2026-05-09 12:56:40 -0500

    modelo para obtener la vista

---
+++ app/models/viewsModel.php
@@ -0,0 +1,24 @@
+<?php
+    
+    namespace app\models;
+
+    class viewsModel{
+        /* MOdelo para obtener las vista */
+
+        protected function obtVisMo($vista){
+            $lisBlan= ["dashboard"];
+
+            if (in_array($vista,$lisBlan)) {
+                if (is_file("app/views/content/".$vista."-view.php")) {
+                    $conte = "app/views/content/".$vista."-view.php";
+                }else{
+                    $conte = "404";
+                }
+            }elseif($vista == "login" || $vista == "index"){
+                $conte="login";
+            }else{
+                $conte="404";
+            }
+            return $conte;
+        }
+    }
