c69e5cdcb7da5d02242c166340e8f0c4592f9445
victor-huallpa <vech.600@gmail.com>
2026-05-10 18:04:59 -0500

    feat: ajax escucha de usuarios

---
+++ app/ajax/usuarioAjax.php
@@ -0,0 +1,14 @@
+<?php
+    require_once "../../config/app.php";
+    require_once "../views/inc/session-start.php";
+    require_once "../../autoload.php";
+
+    use app\controllers\userController;
+
+    if (isset($_POST['modulo_usuario'])) {
+        $insUsu= new userController();
+    } else {
+        session_destroy();
+        header("Loction: ".APP_URL."login/");
+    }
+    
