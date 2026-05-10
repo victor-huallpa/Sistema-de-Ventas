51c2d436263021124535426372f4c01bc579a007
victor-huallpa <vech.600@gmail.com>
2026-05-09 21:50:43 -0500

    refactor: optimizaicon de index.php

--- index.php
+++ index.php
@@ -1,6 +1,17 @@
 <?php
     require_once "config/app.php";
+    require_once "autoload.php";
     require_once "app/views/inc/session_start.php";
+    if (isset($_GET['views'])) {
+
+        $url = explode("/", $_GET['views']);
+
+        // $url = ["categoryupdate", "1", "7"];
+    }else{
+        $url = ["login"];
+    }
+
+    // echo $url[2];
 ?>
 <!DOCTYPE html>
 <html lang="es">
@@ -8,6 +19,20 @@
 <?php require_once "app/views/inc/head.php";?>
 </head>
 <body>
+    <?php
+        use app\controllers\viewsController;
+
+
+        $viewsController= new viewsController;
+        $vista=$viewsController->obtVisCon($url[0]);
+
+        if ($vista=="login" || $vista=="404") {
+            require_once "app/views/content/".$vista."-view.php";
+        } else {
+            
+        
+        
+    ?>
 
     <!-- Main container -->
     <main class="page-container">
@@ -23,10 +48,12 @@
         <section class="full-width pageContent scroll" id="pageContent">
 
             <!-- NavBar -->
-            <?php require_once "app/views/inc/navBar.php";?>
+            <?php 
+                require_once "app/views/inc/navBar.php";
 
-            <!-- Content -->
-            <!-- <?php require_once "app/views/content/dashboard-view.php";?> -->
+            // <!-- Content -->
+                require_once $vista;
+            ?> 
 
             <!-- Fin content -->
 
@@ -34,7 +61,7 @@
         </section> <!-- Fin page content -->
 
     </main> <!-- Fin main container -->
-    <?php require_once "app/views/inc/script.php";?>
+    <?php } require_once "app/views/inc/script.php";?>
 
     
 </body>
