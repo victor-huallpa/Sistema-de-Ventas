1f40d30f803c8effd8aed7f3e93cbc241fb44d3f
victor-huallpa <vech.600@gmail.com>
2026-05-10 18:12:18 -0500

    feat: alerta de sit alert

--- app/views/js/ajax.js
+++ app/views/js/ajax.js
@@ -41,3 +41,39 @@ formularios_ajax.forEach(formularios => {
 
     });
 });
+
+function alertas_ajax(alerta){
+
+    if(alerta.tipo=="simple"){
+        Swal.fire({
+            icon: alerta.icono,
+            title: alerta.titulo,
+            text: alerta.texto,
+            confirmButtonText: 'Aceptar'
+        });
+    }else if(alerta.tipo=="recargar"){
+        Swal.fire({
+            icon: alerta.icono,
+            title: alerta.titulo,
+            text: alerta.texto,
+            confirmButtonText: 'Aceptar'
+        }).then((result) => {
+            if(result.isConfirmed){
+                location.reload();
+            }
+        });
+    }else if(alerta.tipo=="limpiar"){
+        Swal.fire({
+            icon: alerta.icono,
+            title: alerta.titulo,
+            text: alerta.texto,
+            confirmButtonText: 'Aceptar'
+        }).then((result) => {
+            if(result.isConfirmed){
+                document.querySelector(".FormularioAjax").reset();
+            }
+        });
+    }else if(alerta.tipo=="redireccionar"){
+        window.location.href=alerta.url;
+    }
+}
