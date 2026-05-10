3b6916c522434c9d977c79f31e2abf18f17923f5
victor-huallpa <vech.600@gmail.com>
2026-05-10 18:39:28 -0500

    feat: clase caja:

---
+++ app/controllers/cashierController.php
@@ -0,0 +1,13 @@
+<?php
+
+    namespace app\controllers;
+    use app\models\mainModel;
+
+    class cashierController extends mainModel{
+
+        //METODO REGISTAR CAJA
+        public function regisCajCon(){
+        }
+
+
+    }
