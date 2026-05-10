a84b9629b5c183a8c3cf956d6dc799d21f0524db
victor-huallpa <vech.600@gmail.com>
2026-05-10 18:42:03 -0500

    feat: ferificando emtodo con ajax

--- app/ajax/usuarioAjax.php
+++ app/ajax/usuarioAjax.php
@@ -7,6 +7,9 @@
 
     if (isset($_POST['modulo_usuario'])) {
         $insUsu= new userController();
+        if($_POST['modulo_usuario']=="registrar"){
+            echo $insUsu->regisUsuCon();
+        }
     } else {
         session_destroy();
         header("Loction: ".APP_URL."login/");
